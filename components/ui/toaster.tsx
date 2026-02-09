cbf3727a592aa984ee6ef7c19f511815d1dfb88d
Petruxchinedu <dominicpeter494@gmail.com>
2026-02-09 12:23:27 +0100

    fix: remove children from ChakraToaster component

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,40 +1,20 @@
-"use client"
+"use client";
 
 import {
   Toaster as ChakraToaster,
   Portal,
-  Stack,
   createToaster,
-} from "@chakra-ui/react"
+} from "@chakra-ui/react";
 
-// This "toaster" object is what you import in LoginPage to show alerts
 export const toaster = createToaster({
-  placement: "top-end",
+  placement: "bottom-end",
   pauseOnPageIdle: true,
-})
+});
 
 export const Toaster = () => {
   return (
     <Portal>
-      <ChakraToaster toaster={toaster} insetInline={6}>
-        {(toast) => (
-          <Stack gap="1" width="full">
-            {toast.title && (
-              <Box fontWeight="medium" fontSize="sm">
-                {toast.title}
-              </Box>
-            )}
-            {toast.description && (
-              <Box fontSize="xs" color="fg.muted">
-                {toast.description}
-              </Box>
-            )}
-          </Stack>
-        )}
-      </ChakraToaster>
+      <ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }} />
     </Portal>
-  )
-}
-
-// Minimal helper Box if not imported
-const Box = (props: any) => <div {...props} style={{ display: "flex", flexDirection: "column", ...props.style }} />
+  );
+};
